566df7d641979ccc0c9aecbb287e6d67bd03c241
Muriel <Muriel@DESKTOP-MURIEL>
2021-06-12 17:00:21 -0300

    Add project files.

---
+++ ProAgil.API/Controllers/WeatherForecastController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using ProAgil.API.Data;
+using ProAgil.API.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProAgil.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly ProAgilDbContext Context;
+
+        public WeatherForecastController(ProAgilDbContext context)
+        {
+            Context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                List<Evento> eventos = await Context.EVENTO.ToListAsync();
+                return Ok(eventos);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Falha no Banco de Dados");
+            }
+        }
+
+        [HttpGet("{eventoId}")]
+        public async Task<IActionResult> Get(int eventoId)
+        {
+            try
+            {
+                Evento evento = await Context.EVENTO.SingleOrDefaultAsync(evento => evento.EventoId.Equals(eventoId));
+                return Ok(evento);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Falha no Banco de Dados");
+            }
+        }
+    }
+}
---
+++ ProAgil.API/Migrations/20210612193134_Initial_Migration.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProAgil.API.Migrations
+{
+    public partial class Initial_Migration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Evento",
+                columns: table => new
+                {
+                    EventoId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Local = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DataEvento = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Tema = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    QtdPessoas = table.Column<int>(type: "int", nullable: false),
+                    Lote = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Evento", x => x.EventoId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Evento");
+        }
+    }
+}
